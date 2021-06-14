65f62714bbf63de826d2e9941f05cb90f67e31b6
TumeloRakotsoane <79556809+TumeloRakotsoane@users.noreply.github.com>
2021-06-14 23:38:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const _ = require('lodash')
 const axios = require('axios')
 
 // SERVER CONFIG
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5050
 const app = express();
 const server = http.createServer(app).listen(PORT, () => console.log(`Reading on ${ PORT }`))
 
